842ecf56a734609a83730db197a9789bd8de7580
Adinath Khamkar <adinath.khamkar97@gmail.com>
2023-05-31 16:53:30 +0530

    sendary error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, useNavigate} from "react-router-dom"
+import {Link} from "react-router-dom"
 import "./Footer.scss"
 import {FaFacebookSquare} from "react-icons/fa"
 import {FaTwitterSquare} from "react-icons/fa"
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,5 @@
 import React from "react";
 import hero_img1 from "../../assets/summer152kjbk.webp"
-import hero_img2 from "../../assets/Overlays_web_Banner_unleash_1.webp"
-import hero_img3 from "../../assets/Overlays_web_Banner_summer_1.webp"
 import "./Hero.scss"
 
 function Hero() {
